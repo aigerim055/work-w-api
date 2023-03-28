14ae1a7a750cd6ea0639bd972be72fde41e6a5f5
aigerim055 <omurzakova727@gmail.com>
2023-03-28 10:44:21 +0600

    convert

--- script.js
+++ script.js
@@ -1,75 +1,13 @@
 const row = document.querySelector('.row')
-// const languages = document.querySelector('#lang') //.getElementsByTagName('option')
-const lang = document.querySelector('#lang')
-
-
-// console.log(lang.innerText)
-
-//
-// lang.addEventListener('change', (event) => {
-//     let v = event.target.value
-//     console.log(event)
-//
-//     if (v === 'ru') {
-//         fetch('https://restcountries.com/v3.1/region/asia')
-//             .then(response => response.json())
-//             .then(data => {
-//                 data.map(country => {
-//                     // console.log(country)
-//                     row.innerHTML += `
-//                 <div class="col-4 cards">
-//                     <div class="box">
-//                         <div class="card">
-//                             <img src="${country.flags.png}" alt="">
-//                             <div class="card-body">
-//                                 <h5 class="card-title">${country.translations.rus.common}</h5>
-//                                 <p class="card-text">Capital: ${country.capital}</p>
-//                                 <p>Population: ${country.population}</p>
-//                                 <button class="btn btn-dark">
-//                                     <a href='${country.maps.googleMaps}' target="_blank">Show in map</a>
-//                                 </button>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>`
-//                 })
-//             })
-//     } else {
-//         fetch('https://restcountries.com/v3.1/region/asia')
-//             .then(response => response.json())
-//             .then(data => {
-//                 data.map(country => {
-//                     // console.log(country)
-//                     row.innerHTML += `
-//                 <div class="col-4 cards">
-//                     <div class="box">
-//                         <div class="card">
-//                             <img src="${country.flags.png}" alt="">
-//                             <div class="card-body">
-//                                 <h5 class="card-title">${country.translations.cym.common}</h5>
-//                                 <p class="card-text">Capital: ${country.capital}</p>
-//                                 <p>Population: ${country.population}</p>
-//                                 <button class="btn btn-dark">
-//                                     <a href='${country.maps.googleMaps}' target="_blank">Show in map</a>
-//                                 </button>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>`
-//                 })
-//
-//             })
-//     }
-// })
-
-
-
 const select = document.querySelector('#continents');
 const countriesDiv = document.querySelector('#countries');
+const input = document.querySelector('.input')
+// const convert = document.querySelector('#convert')
+const result = document.querySelector('.result')
 
 select.addEventListener('change', () => {
     const region = select.value;
-    console.log(region)
+    // console.log(region)
     let url = 'https://restcountries.com/v3.1/';
 
     if (region !== 'all') {
@@ -80,10 +18,7 @@ select.addEventListener('change', () => {
 
     fetch(url)
         .then((response) => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error('Network response was not ok.');
+            return response.json();
         })
         .then((data) => {
             let countriesHTML = '';
@@ -117,80 +52,20 @@ select.addEventListener('change', () => {
 
 
 
-
-
-
-
-
-// fetch('https://restcountries.com/v3.1/region/asia')
-//     .then(response => response.json())
-//     .then(data => {
-//         data.map(country => {
-//             // console.log(country)
-//             row.innerHTML +=`
-//                 <div class="col-4 cards">
-//                     <div class="box">
-//                         <div class="card">
-//                             <img src="${country.flags.png}" alt="">
-//                             <div class="card-body">
-//                                 <h5 class="card-title">${country.translations.cym.common}</h5>
-//                                 <p class="card-text">Capital: ${country.capital}</p>
-//                                 <p>Population: ${country.population}</p>
-//                                 <button class="btn btn-dark">
-//                                     <a href='${country.maps.googleMaps}' target="_blank">Show in map</a>
-//                                 </button>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>`
-//         })
-//
-//     })
-
-//
-
-// window.addEventListener('load', () => { // Вешаем прослушку на событие load
-//
-//     const currency = { USD: 78, EUR: 85.60, RUB: 1 }; // Устанавливаем курс валют
-//
-//     const state = [ 0, 'USD', 'USD' ]; // Начальное состояние
-//
-//     const result = document.getElementsByClassName('convert_result')[0] // Получаем поле куда будем писать результат
-//
-//     document.getElementById('val')
-//         .addEventListener('input', ({ target }) => listener(target.value, 0)) // Получаем элемент ввода данных и вешаем на него слушатель
-//     document.getElementById('cur1')
-//         .addEventListener('change', ({ target }) => listener(target.value, 1)) // Получаем первый селект и вешаем на него слушатель
-//     document.getElementById('cur2')
-//         .addEventListener('change', ({ target }) => listener(target.value, 2)) // Получаем второй селект и вешаем на него слушатель
-//
-//     const listener = (value, key) => { // Обработчик событий
-//         state[key] = value; // Обновляем состояние
-//         result.innerHTML = summ(...state) // Пишем результат
-//     }
-//
-//     function summ(val, cur1, cur2) { // Делаем функцию
-//         let z = 0;
-//         if(cur1 === cur2){ // Если оба значения в селектах равны
-//             return val; // То просто вписываем данные из поля ввода
-//         } else {
-//             if(cur1 != 'RUB'){ // Если не равны рублю, то
-//                 z = val*currency[cur1]; // Переводим сумму в рубли
-//                 return Math.ceil((z/currency[cur2])*100)/100; // Делим на курс и округляем до сотых
-//             } else { // Если не равны
-//                 return Math.ceil((val*currency[cur2])*100)/100; // Умножаем на курс и округляем до сотых
-//             }
-//         }
-//     }
-//
-// })
-//
-//
-//
-//
-//
-
-
-
+input.addEventListener('input', (event) => {
+    let sum = event.currentTarget.value
+    // console.log(sum)
+    fetch(`https://api.exchangerate.host/latest?base=kgs&amount=${sum}&symbols=USD,EUR`)
+        .then(res => res.json())
+        .then(data => {
+            let a = data.rates
+            console.log(a)
+            result.innerHTML = `
+                <h5>USD: ${a.USD}</h5>
+                <h5>EUR: ${a.EUR}</h5>
+            `
+            console.log(data.rates)
+        })
+})
 
 
